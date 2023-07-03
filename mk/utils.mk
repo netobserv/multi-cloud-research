52db8cfb20f94968a411ae5ae126cb01213e0622
Dushyant <dushyantbehl@hotmail.com>
2023-07-03 17:13:23 +0530

    Make mk visible

--- mk/utils.mk
+++ mk/utils.mk
@@ -1,5 +1,8 @@
 .PHONY: prereqs
+
 prereqs:
 	@echo -e "\n==> Checking if prerequisites are met"
 	@jq --version >/dev/null 2>&1 || (echo -e "\n!!! prereq failed !!!: jq is required.\n"; exit 1)
+	@echo -e "\n==> Creating bin directory: ${BINDIR} if not present"
+	@mkdir -p ${BINDIR}
 	@echo -e "==> Everything is good :-)\n"
